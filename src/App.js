e53809b6874a81bf9da91411621e375f199b7bea
MohammadHader <mhader@atypon.com>
2019-04-15 13:36:53 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,28 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import firebase from "firebase";
+import FileUploader from "react-firebase-file-uploader";
+var config = {
+  apiKey: "AIzaSyB_3zIpSArggwwgfWkOxc9yh9f4BQ3lWUY",
+  authDomain: "edupan-38ad5.firebaseapp.com",
+  databaseURL: "https://edupan-38ad5.firebaseio.com",
+  projectId: "edupan-38ad5",
+  storageBucket: "edupan-38ad5.appspot.com",
+  messagingSenderId: "1034925747541"
+};
 
-class App extends Component {
+// Setup Firebase
+firebase.initializeApp(config);
+
+class App extends React.Component {
+  state = {
+    filenames: [],
+    downloadURLs: [],
+    isUploading: false,
+    uploadProgress: 0
+  };
+
+  handleUploadStart = () =>
+    this.setState({
+      isUploading: true,
+      uploadProgress: 0
+    });
+
+  handleProgress = progress =>
+    this.setState({
+      uploadProgress: progress
+    });
+
+  handleUploadError = error => {
+    this.setState({
+      isUploading: false
+      // Todo: handle error
+    });
+    console.error(error);
+  };
+
+  handleUploadSuccess = async filename => {
+    const downloadURL = await firebase
+      .storage()
+      .ref("images")
+      .child(filename)
+      .getDownloadURL();
+
+    this.setState(oldState => ({
+      filenames: [...oldState.filenames, filename],
+      downloadURLs: [...oldState.downloadURLs, downloadURL],
+      uploadProgress: 100,
+      isUploading: false
+    }));
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <FileUploader
+          accept="image/*"
+          name="image-uploader-multiple"
+          randomizeFilename
+          storageRef={firebase.storage().ref("images")}
+          onUploadStart={this.handleUploadStart}
+          onUploadError={this.handleUploadError}
+          onUploadSuccess={this.handleUploadSuccess}
+          onProgress={this.handleProgress}
+          multiple
+        />
+
+        <p>Progress: {this.state.uploadProgress}</p>
+
+        <p>Filenames: {this.state.filenames.join(", ")}</p>
+
+        <div>
+          {this.state.downloadURLs.map((downloadURL, i) => {
+            return <img key={i} src={downloadURL} />;
+          })}
+        </div>
       </div>
     );
   }
 }
-
 export default App;
